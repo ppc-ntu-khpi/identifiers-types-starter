44fa0e1c2f7d28e2fe6729bbe76afb34cf179f61
Alexander Babich <alexander.taurus@gmail.com>
2020-04-16 17:14:00 +0300

    Add files via upload

---
+++ src/test/TestResult.java
@@ -0,0 +1,12 @@
+package test;
+
+import domain.Exercise;
+
+public class TestResult {
+
+    public static void main(String[] args) {
+
+        int R=10;
+        System.out.println("Area of a circle: "+Exercise.Calculate(R)+"m"+'\u00B2');
+    }
+}
